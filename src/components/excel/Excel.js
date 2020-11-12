956eb465451a701ae23235084faa82640452d85a
belanov-ak <a.k.belanov@gmail.com>
2020-11-12 05:12:57 +0300

    Implemented method of output basic template on the page. Also added scss styles

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,21 @@ export class Excel {
         this.$el = document.querySelector(selector)
         this.components = options.components || []
     }
+
+    //Creates root element and passes value, that stores in toHTML method of each class, to it
+    getRoot() { 
+        const $root = document.createElement('div')
+
+        this.components.forEach(Component => {
+            const component = new Component()
+            $root.insertAdjacentHTML('beforeend', component.toHTML())
+        });
+
+        return $root
+    }
+
+    //Displays the template on the page 
+    render() {
+        this.$el.append(this.getRoot())
+    }
 }
