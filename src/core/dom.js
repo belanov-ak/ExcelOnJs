d04dae79bde8a30b1ea62b559fc167e4a1f0f0f3
belanov-ak <a.k.belanov@gmail.com>
2020-11-16 01:00:10 +0300

    Updeted DOM class. Added methods html, clear, append

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,9 +1,43 @@
-class Dom {
+// Provides flexible interaction with DOM
+class Dom { 
+  constructor(selector) {
+    this.$el = typeof selector === 'string' ?
+    document.querySelector(selector) :
+    selector
+  }
+
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html
+      return this
+    } else {
+      return this.$el.outerHTML.trim()
+    }
+  }
+
+  clear() {
+    this.$el.html('')
+    return this
+  }
+
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el
+    }
+
+    if (Element.prototype.append) {
+      this.$el.append(node)
+    } else {
+      this.$el.appendChild(node)
+    }
+
+    return this
+  }
 }
 
 //method similar to jquery $. Allowing to simplify the code
-export function $() {
-  return new Dom()
+export function $(selector) {
+  return new Dom(selector)
 }
 
 $.create = (tagName, classes = '') => {
@@ -11,5 +45,5 @@ $.create = (tagName, classes = '') => {
   if (classes) {
     el.classList.add(classes)
   }
-  return el
+  return $(el)
 }
